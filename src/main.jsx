bfc35d9ab4502348755d58f2c46e82af2c74a220
shivank mishra <shivankmishra710@gmail.com>
2024-01-30 10:13:45 +0530

    prepare for github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
 import Home from './component/Home/Home';
 import { Layout } from './component/Layout/Layout';
 import About from './component/About/About';
@@ -10,37 +9,18 @@ import Projects from './component/Projects/Projects';
 import CV from './component/CV/CV';
 import ContactUS from './component/ContactUS/ContactUS';
 
-const router =createBrowserRouter(
-  [
-    {
-      path:'/',
-      element:<Layout />,
-      children:[{
-        path:"",
-        element:<Home />
-      },
-      {
-        path:'about',
-        element:<About />
-      },
-      {
-        path:'Projects',
-        element:<Projects />
-      },
-      {
-        path:'CV',
-        element:<CV />
-      },
-      {
-          path:'ContactUS',
-        element:<ContactUS />
-      }]
-    }
-  ]
-);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <RouterProvider  router={router}/>
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="cv" element={<CV />} />
+          <Route path="contactus" element={<ContactUS />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
