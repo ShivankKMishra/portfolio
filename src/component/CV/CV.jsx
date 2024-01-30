bb1b3e9f7e09f23e5b666c8b637222458b6c811b
shivank mishra <shivankmishra710@gmail.com>
2024-01-30 11:47:53 +0530

    pdf path resettting

--- src/component/CV/CV.jsx
+++ src/component/CV/CV.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Import AnnotationLayer styles
-import 'react-pdf/dist/esm/Page/TextLayer.css'; // Import TextLayer styles
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -32,12 +32,10 @@ export default function CV() {
 
   return (
     <div className='flex justify-center'>
-      <Document file="../../public/CV.pdf" onLoadSuccess={onDocumentLoadSuccess} onLoadError={onError}>
-  <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
-</Document>
-      <p>
-        
-      </p>
+      <Document file={`${process.env.PUBLIC_URL}/CV.pdf`} onLoadSuccess={onDocumentLoadSuccess} onLoadError={onError}>
+        <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
+      </Document>
+      <p></p>
     </div>
   );
 }
